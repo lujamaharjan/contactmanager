e66d9b590d0f77146843f8fe8a22f9fc45fb7f30
sachin <sachinmaharjan@acd.edu.np>
2019-10-22 21:55:07 +0545

    Updates

---
+++ precache-manifest.20cfc19b56e31ddfb0ef65890b706b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c86e1920681015e7ac6be84270ea7f",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "8a8e09ccde528946c69a",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2e6e365bf7af9576d245",
+    "url": "/contactmanager/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "8a8e09ccde528946c69a",
+    "url": "/contactmanager/static/js/2.96f380b2.chunk.js"
+  },
+  {
+    "revision": "2e6e365bf7af9576d245",
+    "url": "/contactmanager/static/js/main.16f145a6.chunk.js"
+  },
+  {
+    "revision": "52a8038624ffe69f6d88",
+    "url": "/contactmanager/static/js/runtime-main.9acb0f81.js"
+  }
+]);
